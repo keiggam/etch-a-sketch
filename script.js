114dd3f9868121c5366302dfed90738ae32463d9
keiggam <maggietriestolearn@gmail.com>
2024-08-29 14:14:56 +1000

    Adding grid

---
+++ script.js
@@ -0,0 +1,52 @@
+// https://www.youtube.com/watch?v=JlypmDz6aAU
+
+
+const gridSize = 600; 
+let squaresPerSide = 16; 
+
+
+const sketchArea = document.querySelector("#sketch-area")
+const sliderContainer = document.querySelector("#slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector("#slider-value");
+
+sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`;
+sketchArea.style.width = sketchArea.style.height = `${gridSize}px`;
+
+function setBackgroundColor() {
+    this.style.backgroundColor = 'black';
+}
+
+function createGridCells(squaresPerSide) {
+    const numOfSquares = (squaresPerSide * squaresPerSide);
+    const widthOrHeight = `${(gridSize / squaresPerSide) - 2}px`;
+    for (let i = 0; i < (numOfSquares); i++) {
+        const gridCell = document.createElement("div");
+
+        gridCell.style.width = gridCell.style.height = widthOrHeight;
+        gridCell.classList.add("cell");
+
+        sketchArea.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", setBackgroundColor);
+    }
+}
+
+function removeGridCells(){
+    while(sketchArea.firstChild) {
+        sketchArea.removeChild(sketchArea.firstChild);
+    }
+}
+
+slider.oninput = function () {
+    let txt = `${this.value} X ${this.value} (Resolution)`;
+    sliderValue.innerHTML = txt;
+    removeGridCells();
+    createGridCells(this.value);
+}
+
+createGridCells();
+
+
+
+
